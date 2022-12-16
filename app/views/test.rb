a9c206d386cc3d9155e3feeca9b7659e530b599e
satomi-haruna <satomi.haruna1124@gmail.com>
2022-12-16 14:09:43 +0000

    add

--- app/views/test.rb
+++ app/views/test.rb
@@ -30,3 +30,14 @@
 #   end
 
 # end
+# # # 検索結果、カラータグ　whereで限定して表示する
+#   def search_events
+#     # @event = Event.find(params[:id])
+#     @events = current_user.events.where(color_id: :colorless)
+#     # @events = current_user.events.where(color_id: params[:color_id])
+#     # redirect_to search_events
+#   # 部分テンプレート・Javascriptで
+#   # seachフォームを作成→　color_id　で絞り込み　↑で制限
+#   end
+
+# end
